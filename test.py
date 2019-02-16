ed24be2661b75df484a1bbe924774516319908f4
matt <maber1@binghamton.edu>
2019-02-16 13:55:03 -0500

    began to work on Item constructor

--- test.py
+++ test.py
@@ -4,10 +4,23 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__) # replace with wrapper
 
+def get_name(item_tag):
+    return str(item_tag.find(class_='item-title').contents[0])
+
+def get_cost(item_tag):
+    #return item_tag.find(class_='price-current-label').find('strong').contents[0]
+    return "hello world 1"
+
+def get_savings(item_tag):
+    #return item_tag.find('price-save-percent').contents[0]
+    return "hello world"
+
 class Item:
     def __init__(self, item_tag):
-        self.name = ....
-        self.cost = ....
+        self.name       = get_name(item_tag)
+        self.cost       = get_cost(item_tag)
+        self.savings    = get_savings(item_tag)
+
 
 def generate_items(search):
     words = search.split(' ')
@@ -20,8 +33,11 @@ def generate_items(search):
     return [Item(i) for i in item_containers]
 
 @app.route('/', methods=['GET', 'POST'])
-def index(s=None):
+def index(item_list = []):
     if(request.method == 'POST'):
         s = request.form['text']
         item_list = generate_items(s)
-    return render_template('index.html', s=s)
+        ret = [item.name for item in item_list]
+        return "\n\n".join(ret)
+    else:
+        return render_template('index.html')
