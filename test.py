53e62f0d9143feea3c10d09e1468733bcbe49d00
Nick Vallejos <nvallejos1234@gmail.com>
2019-02-16 15:55:59 -0500

    Added image parsing and displaying

--- test.py
+++ test.py
@@ -4,6 +4,9 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__) # replace with wrapper
 
+def get_image(item_tag):
+    return "http:"+str(item_tag.find('img')['src'])
+
 def get_name(item_tag):
     return str(item_tag.find(class_='item-title').get_text())
 
@@ -18,6 +21,7 @@ def get_savings(item_tag):
 
 class Item:
     def __init__(self, item_tag):
+        self.image      = get_image(item_tag)
         self.name       = get_name(item_tag)
         self.cost       = get_cost(item_tag)
         self.savings    = get_savings(item_tag)
@@ -34,11 +38,12 @@ def generate_items(search):
     return [Item(i) for i in item_containers]
 
 @app.route('/', methods=['GET', 'POST'])
-def index(item_names = [], item_costs = [], item_savings = []):
+def index(item_images = [], item_names = [], item_costs = [], item_savings = []):
     if(request.method == 'POST'):
         s = request.form['text']
         item_list = generate_items(s)
+        item_images     = [item.image for item in item_list]
         item_names      = [item.name for item in item_list]
         item_costs      = [item.cost for item in item_list]
         item_savings    = [item.savings for item in item_list]
-    return render_template('index.html', item_names = item_names, item_costs = item_costs, item_savings = item_savings, length = len(item_names))
+    return render_template('index.html', item_images = item_images, item_names = item_names, item_costs = item_costs, item_savings = item_savings, length = len(item_names))
